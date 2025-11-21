2f188ef053e30d3b8d564b3b69f7e066f106b992
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2025-11-21 09:32:11 +0800

    pip install

--- install_all_dependencies.bat
+++ install_all_dependencies.bat
@@ -1,33 +1,33 @@
 @echo off
 
 
-python -m pip install numpy
-python -m pip install scipy
-python -m pip install astropy
-python -m pip install photutils
-python -m pip install sep
-python -m pip install matplotlib
-python -m pip install opencv-python
-python -m pip install opencv-contrib-python
-python -m pip install scikit-image
-python -m pip install Pillow
-python -m pip install requests
-python -m pip install beautifulsoup4
-python -m pip install pandas
-python -m pip install urllib3
-python -m pip install watchdog
-python -m pip install pathlib2
-python -m pip install pytest
-python -m pip install pytest-cov
+python -m pip install numpy    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install scipy    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install astropy    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install photutils    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install sep    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install matplotlib    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install opencv-python    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install opencv-contrib-python    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install scikit-image    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install Pillow    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install requests    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install beautifulsoup4    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install pandas    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install urllib3    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install watchdog    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install pathlib2    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install pytest    -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install pytest-cov    -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 
-python -m pip install torch torchvision timm
-python -m pip install oss2 py7zr
-python -m pip install gradio
-python -m pip install skyfield
-python -m pip install astroquery
-python -m pip install pympc
-python -m pip install pywin32
+python -m pip install torch torchvision timm   -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install oss2 py7zr   -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install gradio   -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install skyfield   -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install astroquery   -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install pympc   -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install pywin32   -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 
 
